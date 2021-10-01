21a3fe23d16df5a65bc0b9ee16846ecf9724929b
강석현 <tjrgus0713@gmail.com>
2021-10-01 19:39:00 +0900

    Scroll Snap 연습

---
+++ src/Components/Scroll Snap/ScrollSnap.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function ScrollSnap(){
+    const DIV = styled.div`
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: ${props => props.color || 'black'};
+        scroll-snap-align: start;
+        font-size: 64px;
+    `
+    const Container = styled.div`
+        height: 100vh;
+        overflow-y: auto;
+        scroll-snap-type: y mandatory;
+    `
+  return(
+      <Container>
+          <DIV color="red">1</DIV>
+          <DIV color="orange">2</DIV>
+          <DIV color="yellow">3</DIV>
+      </Container>
+  );
+}
+export default ScrollSnap;
---
+++ src/Components/Scroll Snap/ScrollSnap.md
@@ -0,0 +1,15 @@
+## **Scroll Container에 넣는 속성**
+
+- snap-scroll-type
+  - 컨테이너에서 Scroll snap을 할 축과 엄격도를 정한다.
+  - 첫번째에 넣는 값으로는 x축,y축 혹은 block,inline축 등으로 정할 수 있다.
+  - 두번째 넣는 값으로는 mandatory를 넣어서 항상 snap포인트에 놓이도록 강제할 수 있고,
+    proximity를 넣어 사용자의 스크롤을 우선시 할 수 있다.
+- snap-scroll-padding
+
+## 자식에 넣는 속성
+
+- snap-scroll-align
+  - Scroll container가 어디서 Scroll snap해야하는지 정해준다.
+  - none/start/center/end는 각각 지정안함/시작점/중간/끝점으로 지정한다.
+- snap-scroll-margin
